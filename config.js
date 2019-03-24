ca968f9b41f4d7679a2d998f13b1686500139a04
will <22626612@qq.com>
2019-03-24 11:28:16 +0800

    bindings variable from config.js

--- config.js
+++ config.js
@@ -2,5 +2,27 @@ if (process.argv.length == 3) {
     var query = process.argv[2];
     if (query === "libs") {
         console.log(__dirname+'/libs');
+    }else if (query === "includes") {
+        console.log(__dirname+'/includes');
+    }else if (query === "libs_copy") {
+        var os = require('os')
+        var arch = os.arch()
+        var type = os.type() == 'Linux' ? 'linux' : 'win'
+        var libs = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.dll','NpcMpiMonTsp.dll']
+        libs.forEach(lib=>{
+            console.log(`${__dirname}/libs/${type}/${arch}/${lib}`)
+        })
     }
+}
+var addonAPI = require('node-addon-api')
+var os = require('os')
+var arch = os.arch()
+var type = os.type() == 'Linux' ? 'linux' : 'win'
+var libs = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.dll','NpcMpiMonTsp.dll']
+var links = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.lib','NpcMpiMonTsp.lib']
+
+module.exports = {
+    libs2copy:libs.map(lib=>`"${__dirname}/libs/${type}/${arch}/${lib}"`).join(' '),
+    includes_dir:[addonAPI.include,`"${__dirname}/includes/${type}/${arch}/API"`].join(' '),
+    libraries:links.map(lib=>`"${__dirname}/libs/${type}/${arch}/${lib}"`).join(' ')
 }
--- src/jvt.h
+++ src/jvt.h
@@ -1,15 +1,5 @@
-#ifdef __linux__
-    #ifdef __x86_64__
-        #include <linux/x64/API/VideoNetAPI.h>
-    #else
-        #include <linux/ia32/API/VideoNetAPI.h>
-    #endif
-#elif _WIN32
-    #ifdef _WIN64
-        #include <win32/x64/API/VideoNetAPI.h>
-    #else
-        #include <win32/ia32/API/VideoNetAPI.h>
-    #endif
-#else
-#error "Unsupported platform!"
+#ifdef _WIN32
+    #include <windows.h>
 #endif
+
+#include <VideoNetAPI.h>
