eff76a5a7ab86fda1d4549e3ba0f6b7f3a02f25b
will <22626612@qq.com>
2019-03-27 00:34:01 +0800

    windows building

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var addonAPI = require('node-addon-api')
 var os = require('os')
 var arch = os.arch()
 var type = os.type() == 'Linux' ? 'linux' : 'win'
-var libs = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.dll','NpcMpiMonTsp.dll']
-var links = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.lib','NpcMpiMonTsp.lib']
+var libs = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.dll','VideoAnalysisAPI.dll','NpcMpiMonTsp.dll']
+var links = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.lib','VideoAnalysisAPI.lib','NpcMpiMonTsp.lib']
 
 module.exports = {
     libs2copy:libs.map(lib=>`"${__dirname}/libs/${type}/${arch}/${lib}"`).join(' '),
