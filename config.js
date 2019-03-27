9e1bf0230d0c88ffee5741827d8b9c10675ddc5c
signhuwei <22626612@qq.com>
2019-03-27 14:04:16 +0800

    npm published

--- config.js
+++ config.js
@@ -1,10 +1,24 @@
 var addonAPI = require('node-addon-api')
-var os = require('os')
-var arch = os.arch()
-var type = os.type() == 'Linux' ? 'linux' : 'win'
-var libs = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.dll','NpcMpiMonTsp.dll']
-var links = type == 'linux' ? ['libjvtsdk.so','libnpcmpimontsp.so'] : ['VideoNetAPI.lib','NpcMpiMonTsp.lib']
+var osMap = {
+    "Linux":{
+        type:"linux",
+        libs:['libjvtsdk.so','libnpcmpimontsp.so'],
+        links:['libjvtsdk.so','libnpcmpimontsp.so']
+    },
+    "Windowns_NT":{
+        type: "win",
+        libs: ['VideoNetAPI.dll','NpcMpiMonTsp.dll'],
+        links: ['VideoNetAPI.lib','NpcMpiMonTsp.lib']
+    },
+    "Darwin": {
+        type:"linux", //OSX fake to linux
+        libs: ['libjvtsdk.so','libnpcmpimontsp.so'],
+        links: ['libjvtsdk.so','libnpcmpimontsp.so']
+    }
+}
 
+var arch = os.arch()
+var {type,libs,links} = osMap[os.type()];
 module.exports = {
     libs2copy:libs.map(lib=>`"${__dirname}/libs/${type}/${arch}/${lib}"`).join(' '),
     includes_dir:[addonAPI.include,`"${__dirname}/includes/${type}/${arch}/API"`].join(' '),
